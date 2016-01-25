9e97d1fcaf4ebc664843b7197287bb0122ff6bb2
Linus Kipkoech <kipkoechlinuss@berkeley.edu>
2016-01-24 17:44:29 -0800

    first lab

---
+++ Desktop/repo/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+   public static void main(String[] args) {
+   	int num = 1;
+   	while(num < 5){
+   		for(int i = 1; i <= num; i++){
+			System.out.println("*");
+   		}
+   		num ++;
+
+   		}
+
+        
+
+   }
+   	}
